a52d89485680f74d837ec94ba35743b2692ba90f
Allan Bruno <allanbrunorj@gmail.com>
2020-10-10 23:04:29 -0300

    removing print line

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    print([str(post) for post in posts])
     return render(request, 'blog/post_list.html', {'posts': posts})
